f4fd96ba5c56a8a3eea43d8de61c2a6c220f1ca1
helloAmulya <internatomix@gmail.com>
2025-10-26 05:30:46 +0530

    convex autogenerated file

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as teams from "../teams.js";
 import type * as user from "../user.js";
 
 import type {
@@ -25,6 +26,7 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  teams: typeof teams;
   user: typeof user;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
